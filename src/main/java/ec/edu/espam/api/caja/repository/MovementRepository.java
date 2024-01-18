49e32a795af3a514972facff55a950be94505391
Yelro <vanorley20@gmail.com>
2024-01-17 21:12:02 -0500

    Feature: CRUD Movement and Exceptions

--- src/main/java/ec/edu/espam/api/caja/repository/MovementRepository.java
+++ src/main/java/ec/edu/espam/api/caja/repository/MovementRepository.java
@@ -2,7 +2,15 @@ package ec.edu.espam.api.caja.repository;
 
 import ec.edu.espam.api.caja.domain.Movement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MovementRepository extends JpaRepository<Movement, Long> {
+    List<Movement> findByAccountId(Long accountId);
+    @Query(nativeQuery = true, value = "SELECT TOP 1 * FROM MOVEMENT WHERE ACCOUNT_ID = 2 ORDER BY DATE, ID DESC")
+    Optional<Movement> findByAccountOrderByDateDesc(Long accountId);
 }
